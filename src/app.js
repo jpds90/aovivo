c699a72300ad978459ab0bac42107d0c749047fa
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-02-28 16:26:25 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const liveRoutes = require('./routes/liveRoutes');
+const saveRoutes = require('./routes/saveRoutes');
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Rotas
+app.use('/api', liveRoutes);
+app.use('/api', saveRoutes);
+
+module.exports = app;
