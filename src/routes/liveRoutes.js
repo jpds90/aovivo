3476b2529c13ef9d08a27e9b44ec1547adc3e1c7
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-02-28 16:25:48 +0000

    Update liveRoutes.js

--- src/routes/liveRoutes.js
+++ src/routes/liveRoutes.js
@@ -1,1 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
+router.get('/live', async (req, res) => {
+  try {
+    const response = await axios.get('https://v3.football.api-sports.io/fixtures?live=all', {
+      headers: { 'x-apisports-key': process.env.API_KEY },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Erro ao buscar dados ao vivo:', error);
+    res.status(500).send('Erro ao buscar dados ao vivo');
+  }
+});
+
+module.exports = router;
