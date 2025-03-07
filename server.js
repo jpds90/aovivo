3031edd103a7aee4e83b56e5f39f30ea8aaf34a4
jpds90 <123024086+jpds90@users.noreply.github.com>
2025-03-07 13:57:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,14 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
 // Rota para buscar jogos ao vivo da API-Football
 app.get('/api/jogos-ao-vivo', async (req, res) => {
     try {
         const response = await fetch("https://v3.football.api-sports.io/fixtures?live=all", {
             method: "GET",
             headers: {
-                "x-apisports-key": "d6db9473fe5b77e7f299cadd12f2c0bc11111",
+                "x-apisports-key": "d6db9473fe5b77e7f299cadd12f2c0bc",
                 "Content-Type": "application/json"
             }
         });
@@ -28,13 +28,35 @@ app.get('/api/jogos-ao-vivo', async (req, res) => {
         res.status(500).json({ error: "Erro interno no servidor" });
     }
 });
-// Servir a página HTML na raiz
-app.use(express.static('public'));
 
+// Rota para buscar estatísticas dos jogos ao vivo
+app.get('/api/jogo-estatisticas/:fixture_id', async (req, res) => {
+    const { fixture_id } = req.params;  // Pega o fixture_id da URL
+
+    try {
+        const response = await fetch(`https://v3.football.api-sports.io/fixtures/statistics?fixture=${fixture_id}`, {
+            method: "GET",
+            headers: {
+                "x-apisports-key": "d6db9473fe5b77e7f299cadd12f2c0bc",
+                "Content-Type": "application/json"
+            }
+        });
+
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error("Erro ao buscar estatísticas:", error);
+        res.status(500).json({ error: "Erro interno no servidor" });
+    }
+});
+
+app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 // Inicia o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
